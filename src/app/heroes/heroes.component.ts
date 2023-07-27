729978ce11d1910ee2ad4f989298b8af85f1bb68
gunalocal <gunaseelan.k.r.cbe@gmail.com>
2023-07-27 23:49:52 +0530

    sidebar changes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, OnChanges } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroesService } from './heroes.service';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-heroes',
@@ -8,6 +9,14 @@ import { HeroesService } from './heroes.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
+
+
+  private behsub!:BehaviorSubject<any>
+  obs$!:Observable<any>;
+  constructor(){
+   this.behsub=new BehaviorSubject<any>(null);
+   
+  }
 
 
   arra: any[] = ['hari', 'guna', 'velu', 'rathees', 'praveen', 'arun'];
--- src/app/p-2/layout/routing.ts
+++ src/app/p-2/layout/routing.ts
@@ -1,5 +1,4 @@
 import { Routes } from "@angular/router";
-import { HomeComponent } from "src/app/home/home.component";
 
 export const route : Routes=[
    {
