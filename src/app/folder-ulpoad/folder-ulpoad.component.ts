500bf46b603c49c023c277c03d87387168351faf
Guna.KR <guna.k@simbiotiktech.com>
2024-02-21 13:21:06 +0530

    minor fix

--- src/app/folder-ulpoad/folder-ulpoad.component.ts
+++ src/app/folder-ulpoad/folder-ulpoad.component.ts
@@ -12,22 +12,7 @@ export class FolderUlpoadComponent {
     console.log(event.target.files);
   }
 
-  loh() {
-    const fs = require('fs');
-    const path = require('path');
-    const directoryPath = 'D:\\TC\\e-commerce-web\\src\\assets\\media\\svg\\brand-logos';
-
-    fs.readdir(directoryPath, function (err: any, files: any) {
-      if (err) {
-        return console.log('Unable to scan directory: ' + err);
-      }
-
-      // Filter out any non-files (e.g., directories)
-      const fileArray = files.filter((file: any) => fs.statSync(path.join(directoryPath, file)).isFile());
-
-      console.log('Files found:', fileArray);
-    });
-  }
+ 
 
 
 }
