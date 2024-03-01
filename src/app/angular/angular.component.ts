db24bdfa6e7a155e9848d33c2ce53ee36b330f43
Guna.KR <gunaseelan.k.r.cbe@gmail.com>
2024-03-01 11:36:15 +0530

    changes ??

--- src/app/angular/angular.component.ts
+++ src/app/angular/angular.component.ts
@@ -12,49 +12,7 @@ import { HEROES } from '../mock-heroes';
 })
 export class AngularComponent{
 
-  data: any = { name: 'arun', age: 21 };
-  public form: UntypedFormGroup = Object.create(null);
-  message = "I'm read only!";
-  canEdit = false;
-  fontColor = 'blue';
-  sayHelloId = 0;
-
-  parts :any='oader';
-
-  onEditClick() {
-    this.canEdit = !this.canEdit;
-    if (this.canEdit) {
-      this.message = 'You can edit me!';
-    } else {
-      this.message = "I'm read only!";
-    }
-  }
-
-  ab :any;
-
-  constructor(
-    private ref : ElementRef,
-    private fb : FormBuilder
-  ){}
-
-  names =[
-    {name:'guna',age:20,gender:'Male'},
-    {name:'guna',age:20,gender:'Male'},
-    {name:'guna',age:20,gender:'Male'},
-    {name:'guna',age:20,gender:'Male'},
-    {name:'guna',age:20,gender:'Male'},
-    {name:'guna',age:20,gender:'Male'},
-  ]
-  rad!:FormGroup;
-  ngOnInit(): void {
-    this.rad =this.fb.group({
-      nms : [false]
-    });
-   this.ab = new Observable((sub)=>{
-    sub.next(1);
-   });
-   this.ab.pipe(take(2)).subscribe((r:any)=> console.log(r));
-  }
+ 
 
 
 }
