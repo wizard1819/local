db1732ed4cda2833ed7b579e6b42d577da841c08
Guna.KR <gunaseelan.k.r.cbe@gmail.com>
2024-04-11 13:02:08 +0530

    minor fixes

--- src/app/vehicle/update-dialog/update-dialog.component.ts
+++ src/app/vehicle/update-dialog/update-dialog.component.ts
@@ -1,7 +1,7 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { VehicleService } from '../vehicle.service';
-import { UntypedFormControl,UntypedFormGroup,Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, UntypedFormControl, UntypedFormGroup, Validators } from '@angular/forms';
 import { SnackbarService } from 'src/app/snackbar/snackbar.service';
 
 @Component({
@@ -9,79 +9,90 @@ import { SnackbarService } from 'src/app/snackbar/snackbar.service';
   templateUrl: './update-dialog.component.html',
   styleUrls: ['./update-dialog.component.css']
 })
-export class UpdateDialogComponent implements OnInit{
+export class UpdateDialogComponent implements OnInit {
 
 
-  isEdit:boolean=false;
-  public form : UntypedFormGroup = Object.create(null);
-  constructor( 
-    private dialogRef : MatDialogRef<UpdateDialogComponent>,
-    private service : VehicleService,
-    private snackbar : SnackbarService,
-    @Inject (MAT_DIALOG_DATA) public data :any
-  ){
-    this.dialogRef.updateSize("50vw","auto");
+  isEdit: boolean = false;
+  public form: UntypedFormGroup = Object.create(null);
+  constructor(
+    private dialogRef: MatDialogRef<UpdateDialogComponent>,
+    private service: VehicleService,
+    private snackbar: SnackbarService,
+    private fb: FormBuilder,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {
+    this.dialogRef.updateSize("50vw", "auto");
   }
 
- 
+  forms!: FormGroup;
+
   ngOnInit(): void {
+
+    this.forms = this.fb.group({
+      name: ['', [Validators.required]],
+      age: ['', [Validators.required]],
+      gender: ['', [Validators.required]],
+      mobileNo: ['', [Validators.required]],
+    })
+
+
     this.form = new UntypedFormGroup({
-      model : new UntypedFormControl(null, [
+      model: new UntypedFormControl(null, [
         Validators.required,
       ]),
-       brand : new UntypedFormControl(null, [
+      brand: new UntypedFormControl(null, [
         Validators.required,
       ]),
-       owner : new UntypedFormControl(null, [
+      owner: new UntypedFormControl(null, [
         Validators.required,
       ]),
-       status : new UntypedFormControl(null, [
+      status: new UntypedFormControl(null, [
         Validators.required,
       ]),
-       type : new UntypedFormControl(null, [
+      type: new UntypedFormControl(null, [
         Validators.required,
       ]),
-       registerNum : new UntypedFormControl(null, [
+      registerNum: new UntypedFormControl(null, [
         Validators.required,
       ]),
     })
-   this.populateDataOnEdit()
+    this.populateDataOnEdit()
   }
-  populateDataOnEdit(){
-    if(this.data){
-      this.isEdit=true
-      if(this.form.controls!=undefined && this.form.controls!=null){
+  populateDataOnEdit() {
+    if (this.data) {
+      this.isEdit = true
+      if (this.form.controls != undefined && this.form.controls != null) {
         Object.keys(this.form.controls).forEach(key => {
-          if(this.form.controls[key]!=undefined  && this.form.controls[key]!=null){
+          if (this.form.controls[key] != undefined && this.form.controls[key] != null) {
             this.form.controls[key].setValue(this.data[key] || this.data['address'][key]);
           }
         })
       }
     }
   }
- 
 
-  submit(){
-    if(this.form.valid){
+
+  submit() {
+    if (this.form.valid) {
       this.getFunction().subscribe({
-        next : (d) =>{
+        next: (d) => {
           this.snackbar.show(d.message);
         },
-        error: (e)=>{
+        error: (e) => {
           console.log(e);
         },
-        complete : ()=>{
+        complete: () => {
           this.dialogRef.close();
         }
       })
-    }else{
+    } else {
       console.log(this.form.errors);
     }
 
 
   }
 
-  getFunction(){
-    return (this.isEdit!==true)?  this.service.add(this.form.value): this.service.update(this.data.id,this.form.value)
+  getFunction() {
+    return (this.isEdit !== true) ? this.service.add(this.form.value) : this.service.update(this.data.id, this.form.value)
   }
 }
